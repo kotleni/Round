e6cfcb3faf598c62261f0abeb1b215ee686bbf98
kotleni <yavarenikya@gmail.com>
2023-10-28 18:57:42 +0300

    added damaging enemies by velocity

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -73,6 +73,22 @@ public abstract class Entity : MonoBehaviour
         GUI.DrawTexture(new Rect(xPos, yPos, barWidth * healthBarFill, barHeight), Texture2D.whiteTexture, ScaleMode.StretchToFill, false);
     }
 
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Enemy enemy = other.gameObject.GetComponent<Enemy>();
+            if(enemy == null) return;
+
+            float damage = 25f;
+            float damageVelocity = 6.1f;
+            if (_rigidbody.velocity.magnitude >= damageVelocity)
+            {
+                enemy.Hit(damage, gameObject);
+            }
+        }
+    }
+
     private bool CheckIsDamageAnimation()
     {
         return _animator.GetCurrentAnimatorClipInfo(0)[0].clip.name == "PlayerHit";
