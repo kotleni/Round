8f2276f2900350db80a6deef544b6eb50fc4dbe0
kotleni <yavarenikya@gmail.com>
2023-10-28 14:09:30 +0300

    added main menu

---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public void OpenGame()
+    {
+        SceneManager.LoadScene("Scenes/SampleScene");
+    }       
+}
